35b2a56aab569f1d103b736a353fa03b932391c2
Mark Wahl <mwahl@thejakegroup.com>
2016-09-29 01:51:09 -0400

    Initial project commit

---
+++ index.php
@@ -0,0 +1,158 @@
+<?php include "weatherdata.php"; ?>
+
+<!DOCTYPE html>
+<!-- 10K Weather, by Mark Wahl, Copyright 2016 -->
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	
+	<title>10K Weather</title>
+	
+	<meta name="description" content="">
+	
+  <!-- Mobile-friendly viewport -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   <!-- Style sheet link -->
+	<link href="css/main.css" rel="stylesheet" media="all">
+	<link href="css/slider.css" rel="stylesheet" media="all">
+	<link rel='shortcut icon' href='favicon.ico' type='image/x-icon'/ >
+
+</head>
+<body>
+
+	<header role="banner">
+	
+		<a href="index.php"><div id="brand">
+			<div class="logo green"><img src="images/10k.png" alt="10k Weather"></div>
+			<span id="brandname">Weather</span>
+		</div></a>
+		
+		<div id="finder-container">
+			<input type="checkbox" id="finder-toggle"/>  
+			<form id="finder" name="locationFinder" method="get" action="index.php">
+				<input type="text" id="zipcode" name="address" placeholder="Enter Zip Code"><button type="submit" id="findersubmit" value="">&#x27A4;</button>
+			</form>
+			<label class="location-finder" for="finder-toggle">Choose Location</label>  
+		</div>
+
+<!--		<nav role="navigation">
+			<ul class="navbar">
+				<a href="#a1"><li>Today</li></a>
+				<a href="#a2"><li>Tomorrow</li></a>
+				<a href="#a3"><li>7 Day</li></a>
+			</ul>	
+		</nav>
+-->		
+
+	</header>
+
+
+
+	<div id="a1" class="slider-container">
+	    <div id="a2" class="slider-container">
+	        <div id="a3" class="slider-container">
+	            <!-- Top Navigation -->
+	            
+				<nav role="navigation" style="margin-top: -60px; margin-right: 180px;">
+					<ul class="navbar">
+						<a href="#a1" class="test"><li>Today</li></a>
+						<a href="#a2"><li>Tomorrow</li></a>
+						<a href="#a3"><li>7 Day</li></a>
+					</ul>	
+				</nav>
+
+	            <div class="pages">
+	                <!-- First Page #a1 -->
+	                <div id="i1" class="page">
+						<main role="main">
+							
+							<div id="location-time">
+								<h1><?php echo $fullLocation; ?></h1>
+								<?php echo $currentLocLink; ?>
+						   	    <p><?php echo date('l, F jS, Y - g:i A'); ?></p>
+							</div>		
+
+						    <div id="weather-grid">
+						    	<div class="col-100 grid-temp"><?php echo $current_temp; ?>&deg;</div>
+						    	<!-- weather icons from http://www.alessioatzeni.com/meteocons/ -->
+						    	<div class="col-100 grid-hilow"><span style="font-size: 13px; font-weight: normal; margin-left: -5px;">Low - High</span><br/><?php echo $temp_low; ?>&deg; - <?php echo $temp_hi; ?>&deg;</div>
+						    	<div class="col-100 grid-icon"><img src="images/icons/<?php echo $current_icon; ?>.svg" alt="<?php echo $current_weather; ?>"><span style="display: block; margin-bottom: 20px;"><?php echo $current_desc; ?></span></div>
+						    	<div class="grid-forecast""><?php echo $current_forecast; ?></div>
+						    </div>
+
+						    <div class="clear"></div>
+
+						</main><!-- End primary page content -->
+	                </div>
+	                
+	                <!-- Second Page #a2 -->
+	                <div id="i2" class="page">
+						<main role="main">
+							
+							<div id="location-time">
+								<h1><?php echo $city; ?>, <?php echo $state; ?></h1>
+								<?php $datetime = new DateTime('tomorrow'); ?>
+						   	    <p><?php echo $datetime->format('l, F jS, Y'); ?></p>
+							</div>		
+
+						    <div id="weather-grid">
+						    	<div class="col-100 grid-hilow"><span style="font-size: 13px; font-weight: normal; margin-left: -5px;">Low - High</span><br/><?php echo $tomorrow_temp_low; ?>&deg; - <?php echo $tomorrow_temp_hi; ?>&deg;</div>
+						    	<div class="col-100 grid-icon""><img src="images/icons/<?php echo $current_icon; ?>.svg" alt="<?php echo $current_weather; ?>"></div>
+						    	<div class="grid-forecast""><?php echo $tomorrow_weather; ?></div>
+						    </div>
+
+						    <div class="clear"></div>
+
+						</main><!-- End primary page content -->
+	                </div>
+	                
+	                <!-- Third Page #a3 -->
+	                <div id="i3" class="page">
+						<main role="main" class="multiday-table">
+							
+							<div id="location-time">
+								<h1><?php echo $city; ?>, <?php echo $state; ?></h1>
+						   	    <p>7-Day Forecast</p>
+							</div>		
+
+						    <div id="weather-grid">
+
+								<div class="col-100 multiday titles"><div class="period">DAY</div><div class="temp">TEMP</div><div class="pop">PREC</div><div class="weather">FORECAST</div>
+								</div>
+								<?php
+								$a = 0;
+								foreach ($multi_period as &$period) {
+									echo '<div class="col-100 multiday '.$multi_tempLabel[$a].'"><div class="period">';
+									echo $period;
+									echo ", ";
+									echo date("M j", strtotime($multi_date[$a]));
+									echo '</div><div class="temp">';
+									echo $multi_temperature[$a];
+									echo '&deg;f</div><div class="pop">';
+									if ($multi_pop[$a] == '') { echo '0'; }
+									else { echo $multi_pop[$a]; }	
+									echo '%</div><div class="weather">';
+									echo $multi_weather[$a];
+									echo '</div></div>';
+								$a++;
+								}
+
+								?>
+						    </div>
+
+						    <div class="clear"></div>
+
+						</main><!-- End primary page content -->
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+	</div>
+
+	<footer role="contentinfo">
+		<div class="center"><strong>10K WEATHER</strong><br>Providing key weather information where you are, when you need it, without all the extra junk. Copyright &copy; <time datetime="2016">2016</time></div>
+	</footer>
+
+</body>
+</html>
---
+++ weatherdata.php
@@ -0,0 +1,120 @@
+<?php
+
+// GET LOCATION
+
+if ($_GET['address'] != "") {
+	$address = urlencode($_GET['address']);
+	$address_json = file_get_contents("https://maps.googleapis.com/maps/api/geocode/json?address=$address&key=AIzaSyDvFkjTCs4WSoKZwjY5qJ6sWRpNjBWqiLk");
+	$address_obj = json_decode($address_json);
+	$fullLocation = 	$address_obj->results[0]->formatted_address;
+	$lat = $address_obj->results[0]->geometry->location->lat;
+	$lon = $address_obj->results[0]->geometry->location->lng;
+	$currentLocLink = "<a href='index.php' class='currentLink'>Return to Current Location</a>";
+} else {
+	$ip = $_SERVER['REMOTE_ADDR'];
+	if ($ip == '172.148.32.1') { $ip = '68.100.63.75'; }
+	$geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$ip"));
+	// print_r ($geo);
+	$country = $geo["geoplugin_countryName"];
+	$city = $geo["geoplugin_city"];
+	$state = $geo["geoplugin_region"];
+	$fullLocation = $city.', '.$state;
+	$lat = $geo["geoplugin_latitude"];
+	$lon = $geo["geoplugin_longitude"];
+	//$lat = '41.4993';
+	//$lon = '-81.6944';
+}
+
+
+// GET TIMEZONE
+
+$json = file_get_contents("https://maps.googleapis.com/maps/api/timezone/json?location=$lat,$lon&timestamp=1458000000&key=AIzaSyAdmWkyqsySSXrCCvP-4_7MR8XWvC5qFoY");
+
+// echo $json; 
+// var_dump(json_decode($json));
+
+$obj = json_decode($json);
+$user_timezone = $obj->timeZoneId;
+date_default_timezone_set($user_timezone);
+
+// Weather.gov 7-Day
+
+ini_set('user_agent', '10KWeather/1.0.0 (Web Application, 10k.grid.dev)');
+$wgov_json = file_get_contents("http://forecast.weather.gov/MapClick.php?lat=$lat&lon=$lon&unit=0&lg=english&FcstType=json");
+$wgov_obj = json_decode($wgov_json);
+
+$current_temp = $wgov_obj->currentobservation->Temp;
+$current_desc = $wgov_obj->currentobservation->Weather;
+$current_forecast = $wgov_obj->data->text[0];
+$temp_label1 = $wgov_obj->time->tempLabel[0];
+if ($temp_label1 == 'Low') {
+		$temp_low = $wgov_obj->data->temperature[0];
+		$temp_hi = $wgov_obj->data->temperature[1];
+} else {
+		$temp_hi = $wgov_obj->data->temperature[0];
+		$temp_low = $wgov_obj->data->temperature[1];
+}
+
+$clear = array("Sunny","Mostly Sunny","Clear","Mostly Clear","Hot");
+$wind = array("Blowing Dust","Blowing Sand","Windy");
+$ice = array("Patchy Ice Crystals","Areas Ice Crystals","Ice Crystals","Ice Fog","Patchy Freezing Fog","Areas Freezing Fog","Freezing Fog","Freezing Spray","Patchy Frost","Areas Frost","Frost");
+$partlysunny = array("Partly Sunny","Mostly Cloudy","Partly Cloudy","Increasing Clouds","Decreasing Clouds","Becoming Cloudy","Clearing","Gradual Clearing","Clearing Late","Becoming Sunny","A Few Clouds");
+$fog = array("Dense Fog","Fog");
+$partlyfog = array("Patchy Fog","Areas Fog","Patchy Haze","Areas Haze","Haze","Patchy Ice Fog","Areas Ice Fog","Patchy Ash","Areas Ash");
+$smoke = array("Patchy Smoke","Areas Smoke","Smoke","Volcanic Ash");
+$cloudy = array("Cloudy","Cold","Overcast");
+$chancerain = array("Light Rain","Slight Chance Sleet","Chance Sleet","Slight Chance Rain Showers","Chance Rain Showers","Slight Chance Rain","Chance Rain","Slight Chance Drizzle","Chance Drizzle","Drizzle Likely","Drizzle","Slight Chance Rain/Snow","Chance Rain/Snow","Slight Chance Freezing Rain","Chance Freezing Rain","Slight Chance Freezing Drizzle","Chance Freezing Drizzle","Slight Chance Rain/Freezing Rain","Chance Rain/Freezing Rain","Slight Chance Rain/Sleet","Chance Rain/Sleet");
+$rain = array("Sleet Likely","Sleet","Rain Showers Likely","Rain Showers","Rain Likely","Rain","Heavy Rain","Rain/Snow Likely","Rain/Snow","Freezing Rain Likely","Freezing Rain","Freezing Drizzle Likely","Freezing Drizzle","Rain/Freezing Rain Likely","Rain/Freezing Rain","Rain/Sleet Likely","Rain/Sleet");
+$blustery = array("Blustery","Breezy");
+$chancesnow = array("Slight Chance Snow","Chance Snow","Slight Chance Wintry Mix","Chance Wintry Mix","Slight Chance Wintry Mix","Chance Wintry Mix","Slight Chance Snow/Sleet","Chance Snow/Sleet");
+$flurries = array("Slight Chance Snow Showers","Chance Snow Showers","Snow Showers Likely","Snow Showers","Slight Chance Flurries","Chance Flurries","Flurries Likely","Flurries");
+$snow = array("Blowing Snow","Snow Likely","Snow","Blizzard","Wintry Mix Likely","Wintry Mix","Wintry Mix Likely","Wintry Mix","Snow/Sleet Likely","Snow/Sleet");
+$chancethunderstorm = array("Isolated Thunderstorms","Slight Chance Thunderstorms","Chance Thunderstorms","Thunderstorm in Vicinity Light Rain","Thunderstorm in Vicinity");
+$thunderstorm = array("Thunderstorms Likely","Thunderstorms","Severe Tstms","Water Spouts");
+
+// echo $current_desc;
+$current_desc = trim($current_desc);
+
+if (in_array($current_desc, $clear)) { $current_icon = "clear"; }
+elseif (in_array($current_desc, $wind)) { $current_icon = "wind"; }
+elseif (in_array($current_desc, $ice)) { $current_icon = "ice"; }
+elseif (in_array($current_desc, $partlysunny)) { $current_icon = "partlysunny"; }
+elseif (in_array($current_desc, $partlyfog)) { $current_icon = "partlyfog"; }
+elseif (in_array($current_desc, $smoke)) { $current_icon = "smoke"; }
+elseif (in_array($current_desc, $fog)) { $current_icon = "fog"; }
+elseif (in_array($current_desc, $partlyfog)) { $current_icon = "partlyfog"; }
+elseif (in_array($current_desc, $cloudy)) { $current_icon = "cloudy"; }
+elseif (in_array($current_desc, $chancerain)) { $current_icon = "chancerain"; }
+elseif (in_array($current_desc, $rain)) { $current_icon = "rain"; }
+elseif (in_array($current_desc, $blustery)) { $current_icon = "blustery"; }
+elseif (in_array($current_desc, $chancesnow)) { $current_icon = "chancesnow"; }
+elseif (in_array($current_desc, $flurries)) { $current_icon = "flurries"; }
+elseif (in_array($current_desc, $snow)) { $current_icon = "snow"; }
+elseif (in_array($current_desc, $chancethunderstorm)) { $current_icon = "chancethunderstorm"; }
+elseif (in_array($current_desc, $thunderstorm)) { $current_icon = "thunderstorm"; }
+else { $current_icon = "uncertain"; }
+
+// Tomorrow
+
+if ($wgov_obj->time->tempLabel[0] == "Low") { 
+	$tomorrow_temp_hi = $wgov_obj->data->temperature[1];
+	$tomorrow_temp_low = $wgov_obj->data->temperature[2];
+	$tomorrow_weather_short = $wgov_obj->data->weather[1];
+	$tomorrow_weather = $wgov_obj->data->text[1];
+} else {
+	$tomorrow_temp_hi = $wgov_obj->data->temperature[2];
+	$tomorrow_temp_low = $wgov_obj->data->temperature[3];
+	$tomorrow_weather_short = $wgov_obj->data->weather[3];
+	$tomorrow_weather = $wgov_obj->data->text[3];
+}
+
+// MULTIDAY
+
+$multi_period = $wgov_obj->time->startPeriodName;
+$multi_date = $wgov_obj->time->startValidTime;
+$multi_tempLabel = $wgov_obj->time->tempLabel;
+$multi_temperature = $wgov_obj->data->temperature;
+$multi_pop = $wgov_obj->data->pop;
+$multi_weather = $wgov_obj->data->weather;
+
+?>
